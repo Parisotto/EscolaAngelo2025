26e89428e1c74b49c0e52ce3439f3ebf26845eb1
Parisotto <parisotto@gmail.com>
2025-03-18 10:57:03 -0300

    Aula 18/03/2025

--- aula21.py
+++ aula21.py
@@ -20,11 +20,22 @@ print(lista_animais)
 
 # 4. Remova o elemento de índice 2 da lista e mostre o elemento removido.
 lista_nomes = ['Alice', 'Bob', 'Charlie', 'David']
+print(lista_nomes.pop(2))
+
 # 5. Encontre o índice da primeira ocorrência de ‘azul’ na lista.
 lista_cores = ['vermelho', 'azul', 'verde', 'amarelo', 'azul']
+print(lista_cores.index("azul"))
+
 # 6. Conte quantas vezes o número 2 aparece na lista.
 lista_numeros_repetidos = [1, 2, 3, 2, 4, 2, 5, 2]
+print( lista_numeros.count(2))
+
 # 7. Ordene a lista em ordem crescente.
 lista_desordenada = [50, 20, 80, 10, 40]
+lista_desordenada.sort()
+print(lista_desordenada)
+
 # 8. Inverta a ordem dos elementos da lista.
 lista_invertida = ['maçã', 'banana', 'laranja', 'uva']
+lista_invertida.reverse()
+print(lista_invertida)
