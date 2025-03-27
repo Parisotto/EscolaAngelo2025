cbc45ec15a8373527cab2c59affee85c074fd6c7
Parisotto <parisotto@gmail.com>
2025-03-27 08:24:37 -0300

    Aula 27/03/2025

---
+++ aula26.py
@@ -0,0 +1,16 @@
+#  Você está construindo um sistema de gerenciamento de contatos. 
+# Crie um programa que realize as seguintes tarefas:
+
+#  a) Peça ao usuário para digitar seu nome completo.
+
+#  b) Concatene “Olá,” com o nome fornecido e exiba o resultado.
+
+#  c) Conte quantos caracteres existem no nome completo digitado e exiba o resultado.
+
+#  d) Peça ao usuário para digitar um sobrenome para procurar na string do nome completo.
+
+#  e) Verifique se o sobrenome fornecido está presente no nome completo e exiba uma mensagem apropriada.
+
+#  f) Exiba o nome completo em letras maiúsculas.
+
+#  g) Substitua todas as ocorrências da vogal “a” na string do nome completo pelo caractere ‘4’ e exiba o resultado
