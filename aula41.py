bac613ca23af9c228605e18d19b979e60d1477d6
Parisotto <parisotto@gmail.com>
2025-04-24 08:07:17 -0300

    Aula 24/04/2025

---
+++ aula41.py
@@ -0,0 +1,3 @@
+# Faça uma função que retorne um jogo da mega sena com valores aleatórios não repitidos.
+# O usuário deverá dizer quantos números quer jogar, de 6 até 10
+# Imprima o resultado solicitado.
